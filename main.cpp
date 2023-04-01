34c4754d77e5498f7f3d4f1fac63e01855312b5f
Jack David Mullins <jackmullins06@gmail.com>
2023-04-01 20:22:31 +0100

    .

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
+#include <cctype>
+
+std::string makeStringLowerCase(std::string str) {
+    for(int i = 0; i < str.length(); ++i) {
+        str[i] = tolower(str[i]);
+    }
+
+    return str;
+}
 
 void countVowels(std::string input) {
-    int vowels = 0, consenants = 0;
+    int vowels = 0, consonants = 0;
 
     for(int i = 0; i < input.size(); i++) {
         if(input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u')  {
             vowels++;
         } else if(isalpha(input[i])) {
-            consenants++;
+            consonants++;
         }
     }
 
     std::cout << "Number of vowels: " << vowels << std::endl;
-    std::cout << "Number of consenants: " << consenants << std::endl;
+    std::cout << "Number of consenants: " << consonants << std::endl;
 }
 
 int main(int argc, char *argv[]) {
-    countVowels(argv[1]);
+    countVowels(makeStringLowerCase(argv[1]));
     return 0;
 }
