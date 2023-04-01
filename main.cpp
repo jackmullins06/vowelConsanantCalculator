27389f69d1eb446d567b9aadb734ab4a982b4fc0
Jack David Mullins <jackmullins06@gmail.com>
2023-04-01 20:13:14 +0100

    Change the input to parsed through as a command line argument.

--- main.cpp
+++ main.cpp
@@ -1,33 +1,5 @@
 #include <iostream>
 
-
-std::string promptForInput() {
-    std::string input;
-
-    while (true) {
-        std::cout << "Please enter a string: ";
-        std::getline(std::cin, input);
-
-        // Check if input is empty or contains non-alphabetic characters
-        bool invalidInput = input.empty();
-
-        for(int i = 0; i < input.size(); i++) {
-            if(!isalpha(input[i])) {
-                invalidInput = true;
-                break;
-            }
-        }
-
-        if (!invalidInput) {
-            break;
-        }
-
-        std::cout << "Invalid input. Please enter a string containing only alphabetic characters.\n";
-    }
-
-    return input;
-}
-
 void countVowels(std::string input) {
     int vowels = 0, consenants = 0;
 
@@ -43,8 +15,7 @@ void countVowels(std::string input) {
     std::cout << "Number of consenants: " << consenants << std::endl;
 }
 
-int main() {
-    std::string input = promptForInput();
-    countVowels(input);
-
-    return 0;}
+int main(int argc, char *argv[]) {
+    countVowels(argv[1]);
+    return 0;
+}
